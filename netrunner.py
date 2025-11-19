f4a9c2d4cfc86b499dc5d247f1b1a0caae8f14b5
James Rake <jamesrake99@gmail.com>
2025-11-18 20:05:45 -0800

    Added some polish

--- netrunner.py
+++ netrunner.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 gameRunning = True;
 commands = {};
@@ -13,8 +14,15 @@ promptUser = True
 inventory = {}
 
 class Item:
-    def __init__(self, item):
-        +
+    def __init__(self, name, desc, func):
+        self.name = name
+        self.desc = desc
+        self.func = func
+
+    def use(self,params):
+        self.func(params)
+
+docsCyberdeck = Item("Old Cyberdeck", "An old cyberdeck", None)
 
 class Room:
     def __init__(self, description):
@@ -29,15 +37,32 @@ class Room:
         self.nw = None
         self.up = None
         self.down = None
-
+        self.visited = False
+        self.items = {}
+        
+    def getShortDescription(self):
+        plural = ""
+        if len(self.items) > 1:
+            plural = "are"
+        elif len(self.items) == 1:
+            plural = "is"
+        else:
+            return self.desc
+        
+        itemDesc = f"There {plural} "
+        for item in self.items:
+            itemDesc += "a " + item
+        return self.desc + "\n" + itemDesc
+    
 rooms = {
     "reaper" : Room("Reaper's Place"),
     "reaper_basement": Room("Reaper's Basement")
 }
-rooms['reaper'].down = rooms['reaper_basement']
 
+rooms["reaper_basement"].items[docsCyberdeck.name] = docsCyberdeck
+rooms['reaper'].down = rooms['reaper_basement']
 currentRoom = rooms['reaper']
-print(currentRoom.down)
+
 def clear():
     if os.name == 'nt':  # For Windows
         os.system('cls')
@@ -47,12 +72,17 @@ def clear():
 def processCommand(userInput):
     commands[userInput](userInput)
 
+def animPrint(msg):
+    for char in msg:
+        if char == ".":
+            time.sleep(.35)
+        print(char+"", end='', flush=True)
+        time.sleep(.02)
 def processUserInput(userInput):
     if(commands.get(userInput)):
         commands[userInput]['func'](userInput)
         return 0
-    else:
-        return 1
+    return 1
 
 def gameLoop():
     global currentState
@@ -80,22 +110,22 @@ def gameLoop():
                     case "4":
                         print("Quit");
             case "night_city":
-                print("You've finally made it, well sort of. You've reached the famous Night City - a site to behold, a bit different than you're accustomed to, but you've ran out of options. You were made an outcast by your group and after months of scraping by you've finally managed to afford a cheap apartment. Your rent's due by the end of the month and your running low on cash. While crashing at home for the night sounds like heaven, it would be  best to go see a ripperdoc you'll need a cheap cyberdeck if you expect to live out here and netrunning is all you know.\n\n\n\n")
+                animPrint("You've finally made it, well sort of. You've reached the famous Night City - a site to behold, a bit different than you're accustomed to, but you've ran out of options. You were made an outcast by your group and after months of scraping by you've finally managed to afford a cheap apartment. Your rent's due by the end of the month and your running low on cash. While crashing at home for the night sounds like heaven, it would be  best to go see a ripperdoc you'll need a cheap cyberdeck if you expect to live out here and netrunning is all you know.\n\n\n\n")
                 currentPrompt = "Press Enter To Continue"
                 promptUser = True
                 currentState = "ripperdoc"
             case "ripperdoc":
                 if not name:
                     clear()
-                    print("You decide to take a stroll through the district and after a few hours getting acquanted you've gathered that if your looking for quantity, you need to see the doc known as 'The Reaper'. He's known to carry unique hardware and can handle most implants no questions asked. You decide this is the best bet.\nYou arrive at The Reaper's spot - its a pretty unassuming building and isn't visibly marked. You take a beat before reaching for the door. Just as you do the door swings open. 'COME ON NOW I AIN'T GOT ALL DAY' you hear someone shout from the inside. You startle just for a moment before taking a look inside the building. It's dark but you see some dim red lights marking a path down some stairs.\nYou don't waste any time and begin following the path.\nAs you do, you hear the same voice from earlier. The voice is softer now - a low almost calming tone.\n\n'Ahh another runner; not many of us left around here. Don't bother looking for me, I'm out at the moment. What's your name kid? ")
-                    currentPrompt = "Name? "
+                    animPrint("You decide to take a stroll through the district and after a few hours getting acquanted you've gathered that if your looking for quantity, you need to see the doc known as 'The Reaper'. He's known to carry unique hardware and can handle most implants no questions asked. You decide this is the best bet.\nYou arrive at The Reaper's spot - its a pretty unassuming building and isn't visibly marked. You take a beat before reaching for the door. Just as you do the door swings open. 'COME ON NOW I AIN'T GOT ALL DAY' you hear someone shout from the inside. You startle just for a moment before taking a look inside the building. It's dark but you see some dim red lights marking a path down some stairs.\nYou don't waste any time and begin following the path.\nAs you do, you hear the same voice from earlier. The voice is softer now - a low almost calming tone.\n\n'Ahh another runner; not many of us left around here. Don't bother looking for me, I'm out at the moment. What's your name kid? ")
+                    currentPrompt = "\nName? "
                     promptUser = True
                     name = "?"
                 elif(name == "?"):
                     name = resp
                     resp = None
-                    print(name + "'Huh? Looks like your a real nobody kid, but hey that's not the worse thing. Just looking at ya I can tell you don't have any money. Hell I almost pity you. I'll tell you what, since I'm out right now, how about you run the shop for a bit?'\n...'Alrighty then keep heading down to the basement, once you're there go ahead and get setup on my rig. I'll explain the rest once you're in.")
-                    print("\n\n\n\nYou can now use basic movement commands. Use the help command for more info.")
+                    animPrint(name + " Huh? Looks like your a real nobody kid, but hey that's not the worse thing. Just looking at ya I can tell you don't have any money. Hell I almost pity you. I'll tell you what, since I'm out right now, how about you run the shop for a bit?'\n...'Alrighty then keep heading down to the basement, once you're there go ahead and get setup on my rig. I'll explain the rest once you're in.")
+                    animPrint("\n\n\n\nYou can now use basic movement commands. Use the help command for more info.")
                     currentPrompt = "Press Enter To Continue"
                     promptUser = False
                 else:
@@ -105,9 +135,9 @@ def gameLoop():
                 promptUser = True
                 currentPrompt = f"[{name}]: "
                 if(resp is None or status == 0):
-                    print(currentRoom.desc)
+                    print(currentRoom.getShortDescription())
                 elif(status == 1):
-                    print("You can't do that here!")                  
+                    animPrint("You can't do that here!")                  
 def addCommand(name, func, params):
     global commands
     commands[name] = {
